0e85075c69a7d41e837f4758db04c12d3939e7b3
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-19 23:29:07 +0900

    tweetsテーブル、nameカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_19_133239) do
+ActiveRecord::Schema.define(version: 2023_09_19_142636) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
